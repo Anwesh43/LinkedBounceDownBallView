3d41e69fec9baee3edf2b757ab22525cd292f41d
Anwesh43 <anweshthecool0@gmail.com>
2018-09-05 02:40:27 +0530

    adding BDBNode class

--- bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
+++ bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
@@ -97,4 +97,47 @@ class BoundDownBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDBNode(var i : Int, val state : State = State()) {
+        private var next : BDBNode? = null
+        private var prev : BDBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BDBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDBNode {
+            var curr : BDBNode? = this.prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
