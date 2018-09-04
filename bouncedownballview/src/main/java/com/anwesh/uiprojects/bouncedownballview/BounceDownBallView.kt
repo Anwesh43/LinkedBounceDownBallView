783fd78863a2183a0c8c7288b149a02b4406c82f
Anwesh43 <anweshthecool0@gmail.com>
2018-09-05 02:49:12 +0530

    adding BounceDownBall class

--- bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
+++ bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
@@ -140,4 +140,29 @@ class BoundDownBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BounceDownBall(var i : Int) {
+
+        private var root : BDBNode = BDBNode(0)
+        private var curr : BDBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+
+    }
 }
