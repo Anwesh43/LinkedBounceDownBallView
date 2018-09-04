cabc4f186463da00f09cb1de68b053b4a3469402
Anwesh43 <anweshthecool0@gmail.com>
2018-09-05 02:01:37 +0530

    adding libraries and defining nodes

--- bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
+++ bouncedownballview/src/main/java/com/anwesh/uiprojects/bouncedownballview/BounceDownBallView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.bouncedownballview
 /**
  * Created by anweshmishra on 05/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
